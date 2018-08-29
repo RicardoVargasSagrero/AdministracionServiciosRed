f8757bee64c6d39ab58e624d130c4bf8b507bf5f
Alumno <r.sagrero1@gmail.com>
2018-08-29 11:56:34 -0500

    Falta realizar las funciones del protocolo junto con las mascaras para leer los codigos de operacion

--- FTPTprotocol/ftptprotocol.c
+++ FTPTprotocol/ftptprotocol.c
@@ -3,13 +3,31 @@ Con fopen copiar un archivo, de la entrada a la salida
 ese archivo  guardarlo en un arreglo de [512] y 
 despues enviarla por un socket
 Se agrerga funcion para copiar un archivo
+
+Para mayor informacion del protocolo consultar
+https://tools.ietf.org/html/rfc1350
 */
 
 #include <stdio.h>
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <netinet/ip.h> /* superset of previous */
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <stdio.h>
+ #include <arpa/inet.h>
+ #include <sys/time.h>
 
 FILE *fileCopy(FILE *f,char *filename);
+/*Operacion con los oparation codes, simple construccion, del 1 al 5*/
+void ReadRequest();
+void WriteRequest();
+void Data();
+void Acknowledment();
+void Error();
 int main(int argc, char const *argv[])
 {
+
 	FILE *file,*file2;
 	char filename[100],c;
 	printf("Enter the file name to copy: \n");
@@ -21,6 +39,62 @@ int main(int argc, char const *argv[])
 		printf("Error al abrir el archivo");	
 	fclose(file);
 	fclose(file2);
+	/*Funcion para crear un servidor */
+
+ 	struct sockaddr_in local, remota;    
+	int udp_socket,lbind,tam,lrecv,bandera;
+	unsigned char mensaje[1514];
+	struct timeval start, end;
+	long mtime=0, seconds, useconds; 
+
+	udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	if(udp_socket==-1){
+	    perror("\nError al abrir el socket");
+	    exit(1);
+	}
+	else{
+	    perror("\nExito al abrir el socket");
+	    local.sin_family=AF_INET;
+	    local.sin_port=htons(69);
+	    local.sin_addr.s_addr=INADDR_ANY;
+	    lbind=bind(udp_socket,(struct sockaddr *)&local,sizeof(local));
+	    if(lbind==-1)
+	      {
+	      perror("\nError en bind");
+	      exit(1);
+	      }
+	    else
+	      {
+	       printf("\nExito en bind");
+	       lrecv=sizeof(remota);
+	       gettimeofday(&start, NULL);
+	       bandera=0;
+	       while(mtime<5000)
+	       {
+	       tam=recvfrom(udp_socket,mensaje,512,MSG_DONTWAIT,(struct sockaddr*)&remota,&lrecv);
+	       if(tam==-1)
+	        {
+	         perror("\nError al recibir");
+	        }
+	       else
+	        {
+	         printf("\nExito al recibir: %s",mensaje);
+	         bandera=1;
+	         /*EN ESTA PARTE SE REALIZAN LA VALIDACION DE LOS CODIGOS
+	         DE OPERACION PARA ENTRAR A LAS FUNCIONES*/
+	        }
+	        gettimeofday(&end, NULL);
+	        seconds  = end.tv_sec  - start.tv_sec;
+	        useconds = end.tv_usec - start.tv_usec;
+	        mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;
+	        printf("Elapsed time: %ld milliseconds\n", mtime);
+	        if(bandera==1)
+	            break;
+	        }
+	    }
+	}
+	 
+	close(udp_socket);
 	return 0;
 }
 FILE *fileCopy(FILE *f,char *filename){
