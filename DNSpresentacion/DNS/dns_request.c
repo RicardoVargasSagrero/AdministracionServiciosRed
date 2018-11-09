4def80431070f44f00d9b7b40a2034a72954b58d
Ricardo Vargas <r.sagrero1@gmail.com>
2018-11-09 01:23:06 -0600

    Se tiene el analizador de la trama de Codigo de operación, banderas y contadores

--- DNSpresentacion/DNS/dns_request.c
+++ DNSpresentacion/DNS/dns_request.c
@@ -3,6 +3,9 @@ DNS(Domain Name System) uses UDP (User Datagram Protocol) by the port 53
 Consult DNS message
 
 Google DNS (8.8.8.8)
+Consulta 
+http://www.newdevices.com/tutoriales/dns/3.html
+http://www.tcpipguide.com/free/t_DNSMessageHeaderandQuestionSectionFormat.htm
 */
 
 #include <sys/socket.h>
@@ -20,6 +23,8 @@ unsigned char recibe[516];
 int counterInteger = 1;
 void FirstMessage(int,unsigned char[],int,struct sockaddr_in);
 void opcodeOption();
+void flags();
+void contadores();
 int main(){
     struct sockaddr_in local, remota, cliente;
     int udp_socket, lbind, tam, ptam,opcode,lrecv;
@@ -56,14 +61,15 @@ int main(){
             printf("message send\n");
             //while(mtime < 10000){
             	recvfrom(udp_socket,message,516,MSG_DONTWAIT,(struct sockaddr*)&remota,&lrecv);
-              	printf("\tID de transacción\n\t\t%.2X%.2X\n",message[0],message[1]);
+              	printf("-----ID de transacción-----\n\t\t%.2X%.2X\n",message[0],message[1]);
               	if(message[3] && 0x80){
               		printf("\tRespuesta\n");
               	}else{
               		printf("\tSolicitud\n");
               	}
               	opcodeOption();
-
+              	flags();
+              	contadores();
               	gettimeofday(&end, NULL);
             	seconds  = end.tv_sec  - start.tv_sec;
             	useconds = end.tv_usec - start.tv_usec;
@@ -132,9 +138,12 @@ void FirstMessage(int opcode,unsigned char filename[],int udp_socket,struct sock
 void opcodeOption(){
 	//4 bits de codigo de operacion 
 	//Se encuentra en el la posicion 2 
-	/*0x0 Solicitud
-	  0x1 Solicitud inversa
-	  0x2 Solicitud del estado del servidor
+	/*0x0 Solicitud (Query)
+	  0x1 Solicitud inversa (!Query)
+	  0x2 Solicitud del estado del servidor (Status)
+	  0x3 Reservado, no utilizado (Reserverd)
+	  0x4 Notificación  (Notify)
+	  0x5 Actualización (Update)
 	  else not define
 		*/
 	int opcode = 3;
@@ -149,8 +158,110 @@ void opcodeOption(){
 		case 2: 
 			printf("\tSolicitud del estado del servidor\n");
 			break;
+		case 3:
+			printf("\tReservado\n");
+			break;
+		case 4:
+			printf("\tNotificación\n");
+			break;
+		case 5:
+			printf("\tActualización\n");
+			break;
 		default:
-			printf("\tNOT DEFINE\n");
+			printf("\tNo definido\n");
+	}
+}
+void flags(){
+	/*
+	Respuesta de autoridad bandera(Authoritative Answer Flag)
+		This bit is set to indicate that server that created the response
+		is authoritative for the zone in wich the domain name 
+		specified in the Question section is located. if is 0, the 
+		responce is non-authoritative
+
+	Truncado (Truncation Flag)
+		1 si # total de respuestas no podía incluirse 
+
+	Se desea recursión (Recursion Desired)
+		1 Solicitud recursiva
+
+	| -------------- message[3]------| ----message[4]
+		             1 bit        1 nibble             1 bit             1 bit         1 bit
+     -------------------------------------------------------------------------------------------
+    | Solicitud/Respuesta |  Opcode  |   Respuesta de autoridad  | Truncado |Se desea recursion |
+     -------------------------------------------------------------------------------------------
+              1 bit          3 bits(Reservado)    1 nibble
+     -------------------------------------------------------------
+    | Recursion disponible |  0  |   0  |   0  | Codigo de retorno | 
+     -------------------------------------------------------------
+	*/
+	int auxFlag;
+	printf("-----Flags-----\n");
+	if((message[3] >> 2) && 1)
+		printf("\tAutoridad: Es Respuesta de Autoridad\n");
+	else{
+		printf("\tAutoridad: No es Respuesta de Autoridad\n");
+	}
+	if((message[3] >> 1) && 1)
+		printf("\tTruncado: El mensaje esta truncado\n");
+	else{
+		printf("\tTruncado: El mensaje de respuesta esta truncado\n");
+	}
+	if(message[3] && 1)
+		printf("\tRecursión: Es una solicitud recursiva\n");
+	else{
+		printf("\tRecursión: NO es una solicitud recursiva\n");
+	}
+	if(message[4] >> 7)
+		printf("\tRecursión disponible: El servidor puede hacer consultas recurivas\n");
+	else{
+		printf("\tRecursión disponible: El servidor no puede hacer consultas recurivas \n");
+	}
+	auxFlag = (int)(message[4] && 0x0F);
+	printf("\tCódigo de retorno: ");
+	switch(auxFlag){
+		case 0:
+			printf("No ocurrio Error (No error occurred)\n");
+			break;
+		case 1:
+			printf("Error de formato (Format Error)\n");
+			break;
+		case 2:
+			printf("Error en servidor (Server Failure)\n");
+			break;
+		case 3:
+			printf("Error de nombre (Name Error)\n");
+			break;
+		case 4:
+			printf("No implementado, el tipo de consulta noesta implementado (Not implemented)\n");
+			break;
+		case 5:
+			printf("Rechazado (Refused)\n");
+			break;
+		case 6: 
+			printf("Dominio YX \n");		
+			break;
+		case 7:
+			printf("YX RR Set\n");
+			break;
+		case 8:
+			printf("NX RR set\n");	
+			break;
+		case 9:
+			printf("No autorizado por la zona especificada\n");
+			break;
+		case 10:
+		    printf("Not zone: Un nombre especificado en el mensaje no esta en la zona especificada\n");
+		    break;
+		default:
+			printf("ERROR NO DEFINIDO\n");
 			break;
 	}
+}
+void contadores(){
+	printf("-----Contadores------\n");
+	printf("\tContador de peticiones: %d\n", (int)message[5]);
+	printf("\tContador de RR de respuesta: %d\n",(int)message[6]);
+	printf("\tContador de RR de Autoridad: %d\n",(int)message[7]);
+	printf("\tContador de RR adicionales: %d\n",(int)message[8]);	
 }
